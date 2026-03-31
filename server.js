60e1b4668f9133ed0a664492058e28940897d87f
yoljasro <saidaliyevjasur450@gmail.com>
2026-04-01 01:42:33 +0500

    fix: use relative URL for swagger

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ const swaggerOptions = {
     },
     servers: [
       {
+        url: '/',
+        description: 'Current environment'
+      },
+      {
         url: `http://localhost:${process.env.PORT || 5000}`,
+        description: 'Local server'
       },
     ],
     components: {
